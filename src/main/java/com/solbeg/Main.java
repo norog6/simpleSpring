5c500ec7dfcf48b88b84fab41400fd59a61a6031
norog6 <92385720+norog6@users.noreply.github.com>
2023-07-26 15:30:06 +0300

    first commit

--- src/main/java/com/solbeg/Main.java
+++ src/main/java/com/solbeg/Main.java
@@ -1,7 +1,14 @@
 package com.solbeg;
 
+import com.solbeg.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+      var context=new  AnnotationConfigApplicationContext("com.solbeg");
+      var userService =context.getBean(UserService.class);
+        System.out.println(userService.findAll());
+
     }
 }
--- src/main/java/com/solbeg/repository/UserRepository.java
+++ src/main/java/com/solbeg/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.solbeg.repository;
 
 import com.solbeg.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
